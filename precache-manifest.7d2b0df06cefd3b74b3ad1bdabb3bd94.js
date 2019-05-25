c630b8669a4b104f65213234ee86d4f753d5b18f
Frostin <iamfrostin@gmail.com>
2019-05-25 15:00:21 +0530

    Updates

---
+++ precache-manifest.7d2b0df06cefd3b74b3ad1bdabb3bd94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a2927cae18cb3d789ad",
+    "url": "/static/js/main.0a2927ca.chunk.js"
+  },
+  {
+    "revision": "b57280414ddcdf6a01c1",
+    "url": "/static/js/1.b5728041.chunk.js"
+  },
+  {
+    "revision": "0a2927cae18cb3d789ad",
+    "url": "/static/css/main.948342a7.chunk.css"
+  },
+  {
+    "revision": "456ffb5dc6ed54b34631f8bf7dddb5be",
+    "url": "/index.html"
+  }
+];
